599b6b88306688a03e8bfe59ae6473a8ee06fc65
SunoApi <164985398+SunoApi@users.noreply.github.com>
2024-05-31 12:31:57 +0800

    Modified Music Square page song title change to link
    Modified Music Square page song title change to link

---
+++ pages/streamlit_image_select/frontend/build/precache-manifest.f91c973269528bb0fa1739ccec76cf9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36024e4ef5439cc9e0d4001633c4c93f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e25c925ebf3fefb613b",
+    "url": "./static/js/2.c8f0f575.chunk.js"
+  },
+  {
+    "revision": "ea9e8a04b4530945f0b69f09f74b01e4",
+    "url": "./static/js/2.c8f0f575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b27335e2a29da337a3e",
+    "url": "./static/js/main.47dc4d94.chunk.js"
+  },
+  {
+    "revision": "08bfe9a8f9db97a558b1",
+    "url": "./static/js/runtime-main.fce4c018.js"
+  }
+]);
